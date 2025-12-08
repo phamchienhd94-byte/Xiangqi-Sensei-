51124db0e62cc3d7de329489a5e757a9cf2a16c6
My Flutter App <Phamchienhd94@gmail.com>
2025-12-08 20:38:02 +0700

    Fix dong bo ham C++ pikafish_main

--- ios/pikafish_src/main.cpp
+++ ios/pikafish_src/main.cpp
@@ -1,19 +1,6 @@
 /*
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
   Copyright (C) 2004-2025 The Stockfish developers (see AUTHORS file)
-
-  Stockfish is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  Stockfish is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
@@ -28,11 +15,8 @@
 
 using namespace Stockfish;
 
-// --- SỬA ĐỔI CHO IOS ---
-// 1. Đổi tên main -> pikafish_main
-// 2. Thêm extern "C" để tránh đổi tên hàm (name mangling) trong C++
-// 3. Thêm visibility("default") để hàm này hiển thị ra ngoài cho Dart gọi
-extern "C" __attribute__((visibility("default"))) __attribute__((used))
+// --- SỬA ĐỔI: BỎ 'extern "C"', ĐƯA VỀ HÀM C++ THUẦN TÚY ---
+// Để khớp với khai báo trong file pikafish_bridge.cpp
 int pikafish_main(int argc, char* argv[]) {
     std::cout << engine_info() << std::endl;
 
